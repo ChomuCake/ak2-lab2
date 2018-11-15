d5ef98cc8d598b5366b7b67f0806933d0b3207a7
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    edit calculator.cpp and calculator.h
    Add descriptions for functions and header files.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,19 @@
-#include "calculator.h"
+#include "calculator.h" // include header file
 
+// Add
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+// Substraction description
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+        return Add (a, -b);
+}
+
+// Multiplication description
+int Calculator::Mul (double a, double b)
+{
+        return a * b + 0.5;
 }
